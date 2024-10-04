cb7f0d024321ee009e41bcb45869a250aa2bc07f
Paulo Brandão <paulobrandao.dev@outlook.com>
2024-10-04 11:32:56 -0300

    test: Button component
    Close issue #4

---
+++ tests/component-button.test.tsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { describe, expect, test } from 'vitest';
+import { Button } from '../src/lib';
+
+describe('Button component', () => {
+  test('render', () => {
+    const result = render(
+      <>
+        <Button>Default</Button>
+        <Button variant="elevated">Elevated</Button>
+        <Button variant="filled" icon={<span className="pseudo-icon" />}>
+          Filled
+        </Button>
+        <Button variant="outlined">Outlined</Button>
+        <Button as="a" variant="text" data-testid="button_text">
+          Text
+        </Button>
+        <Button variant="tonal">Tonal</Button>
+      </>,
+    );
+    const allDefault = result.getByRole('button', { name: 'Default' });
+    const elevated = result.getByRole('button', { name: 'Elevated' });
+    const filled = result.getByRole('button', { name: 'Filled' });
+    const outlined = result.getByRole('button', { name: 'Outlined' });
+    const text = result.getByTestId('button_text');
+    const tonal = result.getByRole('button', { name: 'Tonal' });
+    expect(allDefault.className).toEqual('material-button-text');
+    expect(elevated.className).toEqual('material-button-elevated');
+    expect(filled.className).toEqual('material-button-filled');
+    expect(filled.firstElementChild).not.toBeNull();
+    expect(outlined.className).toEqual('material-button-outlined');
+    expect(text.tagName).toEqual('A');
+    expect(text.className).toEqual('material-button-text');
+    expect(tonal.className).toEqual('material-button-tonal');
+  });
+});
